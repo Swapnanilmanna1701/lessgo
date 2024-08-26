6e3087fcec53da38792f79ccade7ee753bcd794f
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-08-27 00:03:36 +0530

    preetier error fix

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
+
+const Page = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
+  return <Redirect href="/(auth)/welcome" />;
+};
+
+export default Page;
